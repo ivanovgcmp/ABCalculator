898c036ceb38f697268e07501b28669da891bef2
ivanovgcmp <107063363+ivanovgcmp@users.noreply.github.com>
2022-06-22 19:55:35 +0300

    Adding values to input fields

--- ABCalculator.py
+++ ABCalculator.py
@@ -32,14 +32,16 @@ lblTitle.place(x=25, y=55)
 lblVisit1 = tk.Label(text = 'Visitors', font = ('Helvetica', 10, 'bold'))
 lblVisit1.place(x=25, y=85)
 
-entVisit1 = tk.Entry(font = ('Helvetica', 10, 'bold'))
+entVisit1 = tk.Entry(font = ('Helvetica', 10, 'bold'), justify='center')
 entVisit1.place(x=115, y=85, width=90, height=20)
+entVisit1.insert(tk.END, '0')
 
 lblConvers1 = tk.Label(text = 'Conversion', font = ('Helvetica', 10, 'bold'))
 lblConvers1.place(x=25, y=115)
 
-entConvers1 = tk.Entry(font = ('Helvetica', 10, 'bold'))
+entConvers1 = tk.Entry(font = ('Helvetica', 10, 'bold'), justify='center')
 entConvers1.place(x=115, y=115, width=90, height=20)
+entConvers1.insert(tk.END, '0')
 
 # Adding a Test group header label 
 lblTitle = tk.Label(text = 'Test group', font = ('Helvetica', 12, 'bold'), fg = '#0000cc')
@@ -49,14 +51,16 @@ lblTitle.place(x=25, y=145)
 lblVisit2 = tk.Label(text = 'Visitors', font = ('Helvetica', 10, 'bold'))
 lblVisit2.place(x=25, y=170)
 
-entVisit2 = tk.Entry(font = ('Helvetica', 10, 'bold'))
+entVisit2 = tk.Entry(font = ('Helvetica', 10, 'bold'), justify='center')
 entVisit2.place(x=115, y=170, width=90, height=20)
+entVisit2.insert(tk.END, '0')
 
 lblConvers2 = tk.Label(text = 'Conversion', font = ('Helvetica', 10, 'bold'))
 lblConvers2.place(x=25, y=200)
 
-entConvers2 = tk.Entry(font = ('Helvetica', 10, 'bold'))
+entConvers2 = tk.Entry(font = ('Helvetica', 10, 'bold'), justify='center')
 entConvers2.place(x=115, y=200, width=90, height=20)
+entConvers2.insert(tk.END, '0')
 
 # Adding the Calculate button
 btnProcess = tk.Button(root, text = 'Calculation', font = ('Helvetica', 10, 'bold'), command=popup_window)
